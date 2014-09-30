37c01b5faa0cbad6298af8ac3a08f88abd621d18
kyriosdata <fabio@engenhariadesoftware.inf.br>
2014-09-30 15:04:38 -0300

    Modelagem de domínio (acréscimo de outras classes e interfaces).

---
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/ouvidoria/Assunto.java
@@ -0,0 +1,90 @@
+/*
+ * ====================================================================
+ * Licença da Fábrica de Software (UFG)
+ *
+ * Copyright (c) 2014 Fábrica de Software
+ * Instituto de Informática (Universidade Federal de Goiás)
+ * Todos os direitos reservados.
+ *
+ * Redistribuição e uso, seja dos fontes ou do formato binário
+ * correspondente, com ou sem modificação, são permitidos desde que
+ * as seguintes condições sejam atendidas:
+ *
+ * 1. Redistribuição do código fonte deve conter esta nota, em sua
+ *    totalidade, ou seja, a nota de copyright acima, as condições
+ *    e a observação sobre garantia abaixo.
+ *
+ * 2. Redistribuição no formato binário deve reproduzir o conteúdo
+ *    desta nota, em sua totalidade, ou seja, o copyright acima,
+ *    esta lista de condições e a observação abaixo na documentação
+ *    e/ou materiais fornecidos com a distribuição.
+ *
+ * 3. A documentação fornecida com a redistribuição,
+ *    qualquer que seja esta, deve incluir o seguinte
+ *    texto, entre aspas:
+ *       "Este produto inclui software desenvolvido pela Fábrica
+ *       de Software do Instituto de Informática (UFG)."
+ *
+ * 4. Os nomes Fábrica de Software, Instituto de Informática e UFG
+ *    não podem ser empregados para endoçar ou promover produtos
+ *    derivados do presente software sem a explícita permissão
+ *    por escrito.
+ *
+ * 5. Produtos derivados deste software não podem ser chamados
+ *    "Fábrica de Software", "Instituto de Informática", "UFG",
+ *    "Universidade Federal de Goiás" ou contê-los em seus nomes,
+ *    sem permissão por escrito.
+ *
+ * ESTE SOFTWARE É FORNECIDO "COMO ESTÁ". NENHUMA GARANTIA É FORNECIDA,
+ * EXPLÍCITA OU NÃO. NÃO SE AFIRMA QUE O PRESENTE SOFTWARE
+ * É ADEQUADO PARA QUALQUER QUE SEJA O USO. DE FATO, CABE AO
+ * INTERESSADO E/OU USUÁRIO DO PRESENTE SOFTWARE, IMEDIATO OU NÃO,
+ * ESTA AVALIAÇÃO E A CONSEQUÊNCIA QUE O USO DELE OCASIONAR. QUALQUER
+ * DANO QUE DESTE SOFTWARE DERIVAR DEVE SER ATRIBUÍDO AO INTERESSADO
+ * E/OU USUÁRIO DESTE SOFTWARE.
+ * ====================================================================
+ *
+ * Este software é resultado do trabalho de voluntários, estudantes e
+ * professores, ao realizar atividades no âmbito da Fábrica de Software
+ * do Instituto de Informática (UFG). Consulte <http://fs.inf.ufg.br>
+ * para detalhes.
+ */
+
+package br.ufg.inf.fabrica.muralufg.central.ouvidoria;
+
+import br.ufg.inf.fabrica.muralufg.central.arquivo.Arquivo;
+import org.joda.time.DateTime;
+
+import java.util.Set;
+
+/**
+ * Detalhe de uma submissão dirigida à ouvidoria da UFG.
+ * <p>Uma instância desta classe é um <i>value object</i>.</p>
+ */
+public class Assunto {
+
+    /**
+     * Identifica anexos associados à submissão.
+     * Não é obrigatória a existência de anexo.
+     */
+    private Set<Arquivo> anexos;
+
+    /**
+     * Conteúdo (mensagem) associada à submissão.
+     * Descreve a reclamação, denúncia, elogio ou, de forma
+     * ampla, a mensagem de interesse do emissor.
+     */
+    private String conteudo;
+
+    /**
+     * Data em que o assunto é criado.
+     */
+    private DateTime data;
+
+    /**
+     * Identificação da origem (autor) da mensagem, caso
+     * tenha sido fornecido pelo interessado.
+     * <b>ESTA INFORMAÇÃO NÃO É OBRIGATÓRIA.</b>
+     */
+    private String fonte;
+}
