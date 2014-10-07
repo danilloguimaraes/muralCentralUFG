d5b3c1d2dd12e46016c268fa9aaceccea4347c3c
jober16 <joberguilherme@hotmail.com>
2014-10-07 20:20:54 -0300

    Technical debts removed - Method's name of as convention

--- src/main/java/br/ufg/inf/fabrica/muralufg/central/dominio/Mensagem.java
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/dominio/Mensagem.java
@@ -123,7 +123,7 @@ public class Mensagem {
         }
 
         for (Imagem imagem : imagens) {
-            AdicionaImagem(imagem);
+            adicionaImagem(imagem);
         }
     }
 
@@ -135,7 +135,7 @@ public class Mensagem {
      * @param imagem Informações sobre a imagem a ser
      *               adicionada (associada) à mensagem.
      */
-    private void AdicionaImagem(Imagem imagem) {
+    private void adicionaImagem(Imagem imagem) {
         if (imagem == null) {
             throw new IllegalArgumentException("imagem é null");
         }
