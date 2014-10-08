292755c06fef31c3b05e73cb7e676f27c590b8d6
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-10-07 21:18:13 -0300

    Proxy (definicao dos servicos)

---
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/proxy/ProxyService.java
@@ -0,0 +1,77 @@
+package br.ufg.inf.fabrica.muralufg.central.proxy;
+
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by kyriosdata on 10/7/14.
+ */
+public class ProxyService {
+
+    private RMTCService rmtc;
+
+    private int antiguidadeEmSegundos;
+
+    /**
+     *
+     */
+    private int totalConfortavelDeRespostas;
+
+    private int totalThreadConsumidores;
+
+    /**
+     * Respostas produzidas pela RMTC e "guardadas"
+     * pelo serviço de "proxy" da Central até que
+     * o Cliente da Central faça a requisição por
+     * resposta já disponível.
+     */
+    private HashMap<String, Resposta> respostas;
+
+    /**
+     * Lista de pedidos submetidos pelo Cliente
+     * da Central e que estão aguardando para serem
+     * executados.
+     */
+    private BlockingQueue<Pedido> pedidos;
+
+    public String getResposta(String guid) {
+        Resposta resposta = respostas.get(guid);
+        return resposta.getResposta();
+    }
+
+    /**
+     * Inicia <i>thread</i> (ou tarefa) responsável
+     * por continuamente, conforme período previamente
+     * configurado, "limpar" o depósito de respostas
+     * daquelas "mais antigas".
+     */
+    public void iniciarLimpezaDeAntigos() {
+
+    }
+
+    public void iniciarConsumidores() {
+
+    }
+
+    public void executarRequisicaoRmtc() {
+        while (true) {
+            try {
+                Pedido pedido = pedidos.take();
+
+                // Execute aqui o pedido recuperado!
+                String respostaRmtc = rmtc.getLinha();
+
+            } catch (InterruptedException ie) {
+
+            }
+        }
+    }
+
+    public void limpaAntigas() {
+        for (int i = 0; i < respostas.size(); i++) {
+            //
+        }
+    }
+
+
+}
