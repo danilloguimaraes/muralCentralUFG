55982f3ca95a0f00b989fcfd1a652eefed7fa653
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-10-12 17:57:08 -0300

    Criado as classes AlunoDao, DaoHelper e MainTeste para testar o servico Google Datastore

--- src/main/java/br/ufg/inf/fabrica/muralufg/central/organizacao/Aluno.java
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/organizacao/Aluno.java
@@ -58,4 +58,20 @@ package br.ufg.inf.fabrica.muralufg.central.organizacao;
  */
 public class Aluno {
     private String nome;
+    private String matricula;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
