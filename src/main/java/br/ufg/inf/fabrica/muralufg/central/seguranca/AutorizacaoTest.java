5b31bdcadfba4bb1cba048364842ba138c592ba3
italogmm@gmail.com <italogmm@gmail.com>
2014-10-21 13:15:16 -0200

    Teste (A ser implementado)
    Teste (A ser implementado)

---
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/seguranca/AutorizacaoTest.java
@@ -0,0 +1,11 @@
+package br.ufg.inf.fabrica.muralufg.central.seguranca;
+
+/**
+ * @TODO Aguardar a implementação dos DAO para realização de testes.
+ */
+public class AutorizacaoTest {
+	
+	
+	
+
+}
