e9d9bbc927a836133205bce17022ced7be12c1c5
italogmm@gmail.com <italogmm@gmail.com>
2014-11-18 20:07:20 -0200

    Documentando AcaoEnum.
    Documentando AcaoEnum.

--- src/main/java/br/ufg/inf/fabrica/muralufg/central/seguranca/AcaoEnum.java
+++ src/main/java/br/ufg/inf/fabrica/muralufg/central/seguranca/AcaoEnum.java
@@ -1,5 +1,11 @@
 package br.ufg.inf.fabrica.muralufg.central.seguranca;
 
+/**
+ * 
+ * Enumerado que representa as ações a serem realizadas,
+ * utilizado pela controle de autorização de ações.
+ *  
+ */
 public enum AcaoEnum {
 	
 	ENVIAR_MENSAGEM,
